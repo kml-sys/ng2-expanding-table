e700c0fdc5250abd54432431cb0dfc92d776f20b
Austin Hodge <austin@ract.io>
2017-02-08 14:25:45 -0500

    fix

--- components/ng-table-directives.d.ts
+++
@@ -1,6 +0,0 @@
-import { NgTableComponent } from './table/ng-table.component';
-import { NgTableFilteringDirective } from './table/ng-table-filtering.directive';
-import { NgTablePagingDirective } from './table/ng-table-paging.directive';
-import { NgTableSortingDirective } from './table/ng-table-sorting.directive';
-import { NgTableRowComponent } from './table/ng-table-row.component';
-export declare const NG_TABLE_DIRECTIVES: (typeof NgTableComponent | typeof NgTableFilteringDirective | typeof NgTablePagingDirective | typeof NgTableSortingDirective | typeof NgTableRowComponent)[];
