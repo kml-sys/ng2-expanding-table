40be7dba25f01fe64f8e4c93cdf4c9af3892dcda
Kamel AOUAD <jfnx7678@EB-L-OR1029274.ad-subs.w2k.francetelecom.fr>
2019-06-19 18:13:57 +0200

    clean package

--- demo/index.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-
-if (ENV === 'production') {
-  enableProdMode();
-}
-
-import { Ng2TableDemoModule } from './demo.module';
-platformBrowserDynamic().bootstrapModule(Ng2TableDemoModule);
